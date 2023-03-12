bdc2742b2193cacc88220f2ea50b0d6551b6c23c
Payam Mohajerin Esfahani <payam3006@gmail.com>
2023-03-12 23:47:42 +0330

    functionality ok

--- player.js
+++ player.js
@@ -1,5 +1,11 @@
 const media = document.getElementById("media");
 const play = document.getElementById("play");
+const time = document.getElementById("time");
+const range = document.getElementById("range");
+
+range.max = Number(media.duration);
+const a = console.log;
+a(media.duration);
 
 function playMedia() {
   if (media.paused) {
@@ -12,3 +18,34 @@ const stop = () => {
   media.pause();
   media.currentTime = 0;
 };
+
+const showMin = () => {
+  let min = Math.floor(media.currentTime / 60);
+  if (min < 10) {
+    return `0${min}`;
+  } else {
+    return min;
+  }
+};
+
+function showSecond() {
+  let second = Math.floor(media.currentTime % 60);
+  if (second < 10) {
+    return `0${second}`;
+  } else {
+    return second;
+  }
+}
+
+const showTime = () => {
+  range.value = Number(media.currentTime);
+  time.innerHTML = `${showMin()}:${showSecond()}`;
+};
+
+showTime();
+
+setInterval(showTime, 1000);
+
+range.addEventListener("input", function () {
+  media.currentTime = Number(range.value);
+});
